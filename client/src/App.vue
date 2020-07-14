72f97a70e71f81bf83321d41e35a08a6b7bc52bf
Preil <preil2000@gmail.com>
2020-07-14 17:12:51 +0300

    Horizontal bar created

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,65 @@
 <template>
-  <div>
-    <h1>App</h1>
-    <router-view/>
-  </div>
+    <v-app>
+        <v-card>
+            <!--Horizontal navbar-->
+            <v-toolbar fixed color="primary" dark>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-toolbar-title class="hidden-xs-only">
+                    <router-link to="/" tag="span" style="cursor: pointer">
+                        Vue share
+                    </router-link>
+                </v-toolbar-title>
+
+                <v-spacer></v-spacer>
+
+                <v-text-field
+                        flex
+                        label="Search"
+                        prepend-icon="search"
+                        single-line
+                        hide-details
+                ></v-text-field>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                        class="hidden-xs-only"
+                        flat depressed color="primary"
+                       v-for="item in horizontalNavItems"
+                       :key="item.title"
+                       :to="item.link"
+                >
+                    <v-icon class="hidden-sm-and-down" left>
+                        {{item.icon}}
+                    </v-icon>
+                    {{item.title}}
+                </v-btn>
+            </v-toolbar>
+
+
+        </v-card>
+
+        <!--App content-->
+        <main>
+            <v-container>
+                <router-view/>
+            </v-container>
+        </main>
+    </v-app>
 </template>
+
+<script>
+    export default {
+        name: 'App',
+        computed: {
+            horizontalNavItems() {
+                return [
+                    {icon: 'chat', title: 'Posts', link: '/posts'},
+                    {icon: 'lock_open', title: 'Sign In', link: '/signin'},
+                    {icon: 'create', title: 'Sign Up', link: '/signup'}
+                ]
+
+            }
+        }
+    }
+</script>
 
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
+  <v-container>
     <h1>Home</h1>
-  </div>
+    <v-btn color="primary">Button</v-btn>
+    <v-btn color="success">Success</v-btn>
+    <v-btn color="error">Error</v-btn>
+    <v-btn color="warning">Warning</v-btn>
+    <v-btn color="info">Info</v-btn>
+  </v-container>
 </template>
 
 <script>
