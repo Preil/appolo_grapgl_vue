380d52c65e126b633628b6d0decfc7f36898a7c2
Preil <preil2000@gmail.com>
2020-07-15 10:17:36 +0300

    Side navigation bar added.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,42 @@
 <template>
     <v-app>
         <v-card>
+            <v-navigation-drawer
+                    temporary
+                    fixed
+                    v-model="sideNav"
+            >
+                <v-toolbar class="secondary" flat dark>
+                    <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+                    <v-toolbar-title>
+                        <h1 class="title">Vue share</h1>
+                    </v-toolbar-title>
+                </v-toolbar>
+                <v-divider></v-divider>
+                <v-list>
+                    <v-list-item
+                            v-for="item in sideNavItems"
+                            :key="item.title"
+                            :to="item.link"
+                    >
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                            {{ item.title }}
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-navigation-drawer>
+        </v-card>
+        <v-card>
             <!--Horizontal navbar-->
             <v-toolbar fixed color="primary" dark>
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
                 <v-toolbar-title class="hidden-xs-only">
                     <router-link to="/" tag="span" style="cursor: pointer">
-                        Vue share
+                        <h1 class="title">Vue share</h1>
                     </router-link>
                 </v-toolbar-title>
 
@@ -24,9 +54,9 @@
                 <v-btn
                         class="hidden-xs-only"
                         flat depressed color="primary"
-                       v-for="item in horizontalNavItems"
-                       :key="item.title"
-                       :to="item.link"
+                        v-for="item in horizontalNavItems"
+                        :key="item.title"
+                        :to="item.link"
                 >
                     <v-icon class="hidden-sm-and-down" left>
                         {{item.icon}}
@@ -58,6 +88,24 @@
                     {icon: 'create', title: 'Sign Up', link: '/signup'}
                 ]
 
+            },
+            sideNavItems() {
+                return [
+                    {icon: 'chat', title: 'Posts', link: '/posts'},
+                    {icon: 'lock_open', title: 'Sign In', link: '/signin'},
+                    {icon: 'create', title: 'Sign Up', link: '/signup'}
+                ]
+
+            }
+        },
+        data() {
+            return {
+                sideNav: false
+            }
+        },
+        methods: {
+            toggleSideNav() {
+                this.sideNav = !this.sideNav
             }
         }
     }
