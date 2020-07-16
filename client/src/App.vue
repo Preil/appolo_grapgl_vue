3d5428e572cc386e65675f93799332036a1f89b9
Preil <preil2000@gmail.com>
2020-07-16 13:59:43 +0300

    Components and routes added.

--- client/src/App.vue
+++ client/src/App.vue
@@ -70,8 +70,10 @@
 
         <!--App content-->
         <main>
-            <v-container>
-                <router-view/>
+            <v-container class="mt-4">
+                <transition name="fade">
+                    <router-view/>
+                </transition>
             </v-container>
         </main>
     </v-app>
@@ -111,3 +113,19 @@
     }
 </script>
 
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition-property: opacity;
+        transition-duration: 0.25s;
+    }
+
+    .fade-enter-active {
+        transition-delay: 0.25s;
+    }
+
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0;
+    }
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,20 +2,55 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 
+import AddPost from '../components/Posts/AddPost'
+import Posts from '../components/Posts/Posts'
+import Signin from '../components/Auth/Signin'
+import Signup from '../components/Auth/Signup'
+import Profile from '../components/Auth/Profile'
+
+
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/post/add',
+        name: 'AddPost',
+        component: AddPost
+    },
+    {
+        path: '/posts',
+        name: 'Posts',
+        component: Posts
+    },
+    {
+        path: '/signin',
+        name: 'Signin',
+        component: Signin
+    },
+    {
+        path: '/signup',
+        name: 'Signup',
+        component: Signup
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile
+    },
+
+
+
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
