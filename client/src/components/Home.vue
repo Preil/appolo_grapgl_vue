d3d0354c56fa72e476b4ab8fc9a88013226660f0
Preil <preil2000@gmail.com>
2020-07-18 09:23:20 +0300

    Carousel added.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,29 +1,13 @@
 <template>
-    <v-container>
-        <h1>Home</h1>
+    <v-container text-xs-center>
 
-        <!--<div v-if="$apollo.loading">Loading...</div>-->
-        <!--<ul v-else v-for="post in getPosts" :key="post._id">-->
-            <!--<li>-->
-                <!--{{post.title}} <br>-->
-                <!--{{post.imageUrl}} <br>-->
-                <!--{{post.description}}-->
-            <!--</li>-->
-        <!--</ul>-->
-
-        <ApolloQuery :query="getPostsQuery">
-            <template slot-scope="{ result: {loading, error, data }}">
-                <div v-if="loading">Loading...</div>
-                <div v-else-if="error">Error!{{error.message}}</div>
-                <ul v-else v-for="post in data.getPosts" :key="post._id">
-                    <li>
-                        {{post.title}} {{post.description}} {{post.likes}}
-                    </li>
-                </ul>
-            </template>
-
-        </ApolloQuery>
-
+        <v-flex xs12>
+            <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+                <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+                    <h1 id="carousel__title">{{post.title}}</h1>
+                </v-carousel-item>
+            </v-carousel>
+        </v-flex>
 
 
     </v-container>
@@ -34,21 +18,32 @@
 
     export default {
         name: 'Home',
-        data(){
-            return {
-                getPostsQuery: gql `
-                    query {
+        apollo: {
+            getPosts: gql `
+                query {
                         getPosts {
                             _id
                             title
                             imageUrl
                             description
                         }
-                     }
-                `
-            }
+                }
+            `
 
         }
     }
 </script>
 
+<style>
+    #carousel__title {
+        position: absolute;
+        background-color: rgba(0,0,0,0.5);
+        color: white;
+        border-radius: 5px 5px 0 0;
+        padding: 0.5em;
+        margin: 0 auto;
+        bottom: 50px;
+        left: 0;
+        right: 0;
+    }
+</style>
